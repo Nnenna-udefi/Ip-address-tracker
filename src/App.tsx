5b219850497df8b85cf319d15e39eaa814489dbd
Nnenna Udefi <miriamudefi@gmail.com>
2024-05-02 15:15:08 +0100

    feat: implemented the map display

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { MapResult } from "./component/map";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [locationData, setLocationData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_n0HzQ7ouJcR7ravgnyRwIyMNh0UpE&ipAddress=${inputValue}`
+      );
+      const data = await response.json();
+      setLocationData(data);
+    } catch (error) {
+      console.error("Error fetching data", error);
+    }
+  };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetchData();
+  };
+
   return (
     <div>
       <header className="flex flex-col items-center bg-mobile-bg shadow shadow-black bg-cover bg-no-repeat py-6 w-full  md:bg-desktop-bg">
         <h1 className="text-white text-2xl font-semibold">
           Ip Address Tracker
         </h1>
-        <div className="relative my-6">
-          <input
-            type="text"
-            placeholder="Search for any Ip address or domain"
-            className="border rounded-xl md:w-96 w-full  px-4 py-2 outline-none"
-          />
+        <form onSubmit={handleSubmit}>
+          <div className="relative my-6">
+            <input
+              type="text"
+              placeholder="Search for any Ip address or domain"
+              className="border rounded-xl md:w-96 w-full  px-4 py-2 outline-none"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
 
-          <img
-            src="/images/icon-arrow.svg"
-            alt="arrow"
-            className="absolute top-0.5 -mt-0.5 bg-black px-4 py-3.5 rounded-e-lg  right-0"
-          />
-        </div>
+            <button type="submit">
+              <img
+                src="/images/icon-arrow.svg"
+                alt="arrow"
+                className="absolute top-0.5 -mt-0.5 bg-black px-4 py-3.5 rounded-e-lg  right-0"
+              />
+            </button>
+          </div>
+        </form>
       </header>
+      <main>
+        <MapResult locationData={locationData} />
+      </main>
     </div>
   );
 }
--- src/component/map.tsx
+++ src/component/map.tsx
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-export const Map = () => {
-  return <div>Map</div>;
+type MapProps = {
+  locationData: {
+    ip: string;
+    location: {
+      country: string;
+      region: string;
+      timezone: string;
+      city: string;
+      lat: number;
+      lng: number;
+      postalCode: string;
+      geonameId: number;
+    };
+    domains: string[];
+    as: {
+      asn: number;
+      name: string;
+      route: string;
+      domain: string;
+      type: string;
+    };
+    isp: string;
+  } | null;
+};
+
+const customIcon = L.icon({
+  iconUrl: "../public/images/icon-location.svg",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+export const MapResult = ({ locationData }: MapProps) => {
+  useEffect(() => {
+    let map: L.Map;
+    if (locationData) {
+      try {
+        const latitude = locationData.location.lat;
+        const longitude = locationData.location.lng;
+        map = L.map("mapId").setView([latitude, longitude], 13);
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution:
+            '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
+
+        L.marker([longitude, latitude], { icon: customIcon })
+          .addTo(map)
+          .bindPopup(
+            `Location: ${locationData.location.country}, ${locationData.location.region}<br>
+             Timezone: ${locationData.location.timezone}<br>
+             ISP: ${locationData.isp}<br>
+             ASN: ${locationData.as.asn}`
+          )
+          .openPopup();
+        console.log("map result", map);
+      } catch (error) {
+        console.error("Error displaying map", error);
+      }
+    }
+    return () => {
+      if (map) {
+        map.remove();
+      }
+    };
+  }, [locationData]);
+
+  console.log("location data", locationData);
+
+  return <div id="mapId" className="h-96"></div>;
 };
