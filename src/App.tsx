166e7e7fc98b3acf0dc5d11840312283352690f6
Nnenna Udefi <miriamudefi@gmail.com>
2024-05-03 14:34:41 +0100

    fix: map road display not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { MapResult } from "./component/map";
 
@@ -6,25 +6,36 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [locationData, setLocationData] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        `https://geo.ipify.org/api/v2/country,city?apiKey=at_n0HzQ7ouJcR7ravgnyRwIyMNh0UpE&ipAddress=${inputValue}`
-      );
-      const data = await response.json();
-      setLocationData(data);
-    } catch (error) {
-      console.error("Error fetching data", error);
-    }
-  };
+  const apiKey = process.env.REACT_APP_GEOLOCATION_API_KEY;
+
+  const fetchData = useCallback(
+    async (ipAddress: string) => {
+      try {
+        const response = await fetch(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`
+        );
+        const data = await response.json();
+        setLocationData(data);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      }
+    },
+    [apiKey]
+  );
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetchData();
+    fetchData(inputValue);
   };
+
+  useEffect(() => {
+    const defaultIPAddress = "8.8.8.8";
+    fetchData(defaultIPAddress);
+  }, [fetchData]);
 
   return (
     <div>
@@ -37,7 +48,7 @@ function App() {
             <input
               type="text"
               placeholder="Search for any Ip address or domain"
-              className="border rounded-xl md:w-96 w-full  px-4 py-2 outline-none"
+              className="border rounded-xl md:w-96 w-full text-dark_grey px-4 py-2 outline-none"
               value={inputValue}
               onChange={handleInputChange}
             />
@@ -46,7 +57,7 @@ function App() {
               <img
                 src="/images/icon-arrow.svg"
                 alt="arrow"
-                className="absolute top-0.5 -mt-0.5 bg-black px-4 py-3.5 rounded-e-lg  right-0"
+                className="absolute top-0.5 -mt-0.5 bg-dark_grey px-4 py-3.5 rounded-e-lg  right-0"
               />
             </button>
           </div>
@@ -55,6 +66,18 @@ function App() {
       <main>
         <MapResult locationData={locationData} />
       </main>
+      <footer className="bg-dark_grey py-8 text-white text-center">
+        <p>
+          Built with love by{" "}
+          <a
+            href="https://github.com/Nnenna-udefi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nnenna Udefi
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
--- src/component/map.tsx
+++ src/component/map.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
-import "leaflet/dist/leaflet.css";
+
 import L from "leaflet";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 type MapProps = {
   locationData: {
@@ -29,11 +30,13 @@ type MapProps = {
 
 const customIcon = L.icon({
   iconUrl: "./images/icon-location.svg",
-  iconSize: [20, 32],
+  iconSize: [30, 38],
   iconAnchor: [16, 32],
   popupAnchor: [0, -32],
 });
 export const MapResult = ({ locationData }: MapProps) => {
+  const mapAccessToken = process.env.REACT_APP_MAP_API_KEY;
+
   useEffect(() => {
     let map: L.Map;
     if (locationData) {
@@ -41,22 +44,17 @@ export const MapResult = ({ locationData }: MapProps) => {
         const latitude = locationData.location.lat;
         const longitude = locationData.location.lng;
         map = L.map("mapId").setView([latitude, longitude], 13);
-        L.tileLayer(
-          "https://{s}.tile.openstreetmap.transport.opendata.city/{z}/{x}/{y}.png",
-          {
-            attribution:
-              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            maxZoom: 18,
-          }
-        ).addTo(map);
+        L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, {
+          maxZoom: 18,
+          attribution: "",
+        }).addTo(map);
 
         L.marker([longitude, latitude], { icon: customIcon })
           .addTo(map)
           .bindPopup(
-            `Location: ${locationData.location.country}, ${locationData.location.region}<br>
-             Timezone: ${locationData.location.timezone}<br>
-             ISP: ${locationData.isp}<br>
-             ASN: ${locationData.as.asn}`
+            `${locationData.location.country}, ${locationData.location.region}<br>
+            
+             ${locationData.ip}`
           )
           .openPopup();
       } catch (error) {
@@ -68,37 +66,68 @@ export const MapResult = ({ locationData }: MapProps) => {
         map.remove();
       }
     };
-  }, [locationData]);
+  }, [locationData, mapAccessToken]);
 
   return (
     <div className="relative">
-      <div className="absolute font-semibold text-dark_grey -mt-10 z-10 ml-20 bg-white p-4 text-2xl flex justify-between rounded-lg w-3/4">
-        <div>
-          <h3 className="uppercase text-light_grey text-xs py-2 font-semibold">
-            Ip Address
-          </h3>
-          <p>{locationData?.ip}</p>
-        </div>
-        <div className="border-l-2 pl-3">
-          <h3 className="uppercase font-semibold text-light_grey text-xs py-2">
-            Location
-          </h3>
-          <p>{`${locationData?.location.city}, ${locationData?.location.country} ${locationData?.as.asn}`}</p>
-        </div>
-        <div className="border-l-2 pl-3">
-          <h3 className="uppercase font-semibold text-light_grey text-xs py-2">
-            Timezone
-          </h3>
-          <p>{locationData?.location.timezone}</p>
-        </div>
-        <div className="border-l-2 pl-3">
-          <h3 className="uppercase font-semibold text-light_grey text-xs py-2">
-            Isp
-          </h3>
-          <p>{locationData?.isp}</p>
+      <div className="flex justify-center">
+        <div className="absolute font-semibold md:text-left text-center text-dark_grey -mt-10 z-10 bg-white p-4 text-2xl flex md:flex-row flex-col justify-between rounded-lg w-3/4">
+          <div>
+            <h3 className="uppercase text-light_grey text-xs py-2 font-semibold">
+              Ip Address
+            </h3>
+            <p>{locationData?.ip}</p>
+          </div>
+          <div className="md:border-l-2 md:pl-3 pl-0 md:py-1 py-2 border-l-0">
+            <h3 className="uppercase font-semibold text-light_grey text-xs py-2">
+              Location
+            </h3>
+            <p>{`${locationData?.location.city}, ${locationData?.location.country} ${locationData?.as.asn}`}</p>
+          </div>
+          <div className="md:border-l-2 md:pl-3 pl-0 md:py-1 py-2 border-l-0">
+            <h3 className="uppercase font-semibold text-light_grey text-xs py-2">
+              Timezone
+            </h3>
+            <p>{locationData?.location.timezone}</p>
+          </div>
+          <div className="md:border-l-2 md:pl-3 pl-0 md:py-1 py-2 border-l-0">
+            <h3 className="uppercase font-semibold text-light_grey text-xs py-2">
+              Isp
+            </h3>
+            <p>{locationData?.isp}</p>
+          </div>
         </div>
       </div>
-      <div id="mapId" className="h-[490px] "></div>
+      <div id="mapId" className="h-96 ">
+        {/* <MapContainer
+        center={
+          locationData
+            ? [locationData.location.lat, locationData.location.lng]
+            : [-1.286389, 36.817223]
+        }
+        zoom={13}
+        className="h-[100vh]"
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {locationData && (
+          <Marker
+            position={[
+              locationData.location.lat,
+              locationData.location.lng,
+            ]}
+            icon={customIcon}
+          >
+            <Popup>
+              {locationData.location.country}, {locationData.location.city}
+            </Popup>
+          </Marker>
+        )}
+      </MapContainer> */}
+      </div>
     </div>
   );
 };
